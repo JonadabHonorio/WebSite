3514344b83bad02194c1a8fc9e363d5baf3327ce
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2023-07-19 19:36:58 -0300

    Add files via upload

---
+++ scriptWeb.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    if (container_menu.style.display == "block") {
+        container_menu.style.display = "none"
+    } else {
+        container_menu.style.display = "block"
+    } 
+          
+};
