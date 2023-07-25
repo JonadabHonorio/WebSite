0c1d94472195df45285f7b700e6806af1b6013a7
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2023-07-24 21:00:29 -0300

    Add files via upload

--- scriptWeb.js
+++ scriptWeb.js
@@ -1,13 +1,16 @@
 const menu = document.getElementById("Menu");
-const botaoMenu = document.getElementById("icone_menu");
-
-function clickMenu() {           /*Função responsável por revelar e ocultar o menu lateral juntamente com a mudança de cor do botão */
+const botaoMenu = document.getElementById("botao_menu");
+function clickMenu() {        /*Função responsável por revelar e ocultar o menu lateral juntamente com a mudança de cor do botão */
     if (menu.style.display == "block" ) {
         menu.style.display = "none"
         botaoMenu.style.color = "#FF6000"
+        menu.style.animation = "saida 0.3s ease-in"
     } else {
         menu.style.display = "block"
         botaoMenu.style.color = "#F0F0F0"
+        menu.style.animation = "start .3s ease-in"
+        
+          
     }   
 }; 
 
