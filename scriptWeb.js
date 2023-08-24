731613ef53161235af2eaea873efc088a5e6c00a
JonadabHonorio <96204594+JonadabHonorio@users.noreply.github.com>
2023-08-24 15:17:57 -0300

    Add files via upload

--- scriptWeb.js
+++ scriptWeb.js
@@ -2,7 +2,7 @@ const menu = document.getElementById("Menu");
 const botaoMenu = document.getElementById("botao_menu");
 
 function clickMenu() {        /*Função responsável por revelar e ocultar o menu lateral juntamente com a mudança de cor do botão */
-    if (menu.style.display == "block" ) {
+    if (menu.style.display == "block") {
         menu.style.display = "none"
         botaoMenu.style.color = "#FF6000"
         
